e1879edc13e60c3133855a184413e837c17b8c59
KPreetha <47448806+KPreetha@users.noreply.github.com>
2020-07-09 18:48:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -30,10 +30,6 @@ def speechtotext(text):
 
 app = Flask(__name__)
 @app.route("/")
-def login():
-    return render_template("login.html")
-    
-@app.route("/home")
 def home():
     return render_template("index.html")
 
